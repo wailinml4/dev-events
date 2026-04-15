0ac1e1c5fd1c5df3ae5b01c2a8dc109af4d2c7d0
wailinse4 <wailinse4@gmail.com>
2026-04-15 10:43:06 -0400

    feat: implement cloudinary image upload

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '@/lib/db';
 import { Event } from '@/models/event.model';
 
+import { v2 as cloudinary } from 'cloudinary';
+
 export async function POST (req: NextRequest) {
     try {
         await connectDB() 
@@ -16,7 +18,28 @@ export async function POST (req: NextRequest) {
             return NextResponse.json({ message: 'Invalid form data' }, { status: 400 })
         }
 
+        const file = formData.get('image') as File 
+        if (!file) {
+            return NextResponse.json({ message: 'Image file is required' }, { status: 400 })
+        }
+
+        const arrayBuffer = await file.arrayBuffer()
+        const buffer = Buffer.from(arrayBuffer)
+
+        const uploadResult = await new Promise((resolve, reject) => {
+            const stream = cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'DevEvents' }, (error, results) => {
+                if (error) {
+                    return reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+            stream.end(buffer);
+        });
+
+        event.image = (uploadResult as { secure_url: string}).secure_url;
         const createdEvent = await Event.create(event)
+
         return NextResponse.json({ message: 'Event created successfully', event: createdEvent }, { status: 201 })
 
     }
